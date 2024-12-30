b089e04cf259f51ea3f406bcaeb0a202948dcc5f
Alam Guardin <alamguardin@gmail.com>
2024-12-29 20:49:51 -0500

    feat: show eval results in the DOM

--- app/main.js
+++ app/main.js
@@ -4,9 +4,13 @@ import { EditorView, keymap } from '@codemirror/view';
 import { defaultKeymap } from '@codemirror/commands';
 import { javascript } from '@codemirror/lang-javascript';
 import { dracula } from 'thememirror';
+import * as babel from '@babel/standalone';
 
+/**
+ * Get Started the editor
+ */
 const startState = EditorState.create({
-	doc: 'const gretting = "Hello"; \nconsole.log(gretting);',
+	doc: '//Write something',
 	extensions: [keymap.of(defaultKeymap), javascript(), dracula],
 });
 
@@ -15,6 +19,35 @@ const view = new EditorView({
 	parent: document.querySelector('#editor'),
 });
 
-document.querySelector('#editor').addEventListener('keyup', () => {
-	//???
+const editor = document.getElementById('editor');
+const output = document.getElementById('output');
+
+editor.addEventListener('keyup', () => {
+	try {
+		const code = view.state.doc.toString();
+		const astCode = babel.packages.parser.parse(code);
+		const nodes = astCode.program.body;
+
+		let dynamicCode = '';
+		let resultsFragment = '';
+		const resutls = [];
+
+		for (const node of nodes) {
+			const generateCode = babel.packages.generator.default(node);
+			dynamicCode += generateCode.code;
+			if (node.type === 'ExpressionStatement') {
+				// biome-ignore lint/security/noGlobalEval: <explanation>
+				resutls.push(eval(dynamicCode));
+			}
+		}
+
+		for (const result of resutls) {
+			const resultExpression = `<div>${result}</div>`;
+			resultsFragment += resultExpression;
+		}
+
+		output.innerHTML = resultsFragment;
+	} catch (e) {
+		output.innerHTML = `<div>${e.name}</div>`;
+	}
 });
